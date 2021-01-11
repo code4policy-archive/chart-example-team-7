42899e5065a34284dd7fe8a6235ceb47dcca8d49
QuentinGollier <quentin.gollier@gmail.com>
2021-01-11 10:15:54 -0500

    exercise 2

--- scripts/chart.js
+++ scripts/chart.js
@@ -30,7 +30,8 @@ var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("id", "apple-stock-chart");
 
 d3.tsv("data.tsv", function(error, data) {
   if (error) throw error;
